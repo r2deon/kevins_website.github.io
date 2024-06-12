272f9973959424819d43b2829e99cd6271443cc0
r2deon <kevschuster7@gmail.com>
2024-06-12 09:58:37 +0200

    Add toggle function

--- js/main.js
+++ js/main.js
@@ -30,7 +30,23 @@ function linkAction(){
 navLink.forEach(n => n.addEventListener('click', linkAction))
 
 /*======================== Accordion Skills ===========================*/
+const skillsContent = document.getElementsByClassName('skills__content'),
+      skillsHeader = document.querySelectorAll('.skills__header')
 
+function toggleSkills(){
+    let itemClass = this.parentNode.className
+
+    for(i = 0; i < skillsContent.length; i++){
+        skillsContent[i].className = 'skills__content skills__close'
+    }
+    if(itemClass === 'skills__content skills__close'){
+        this.parentNode.getElementsByClassName = 'skills__content skills__open'
+    }
+}
+
+skillsHeader.forEach((el) =>{
+    el.addEventListener('click', toggleSkills)
+})
 
 /*======================== Qualifcation Tabs ==========================*/
 
